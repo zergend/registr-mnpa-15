2045dd086c2d740342ae67ed3265ea6152c654bc
zergend <zergend@gmail.com>
2015-05-11 22:33:31 +0300

    изменил кое-что

--- includes/layouts/sidebar_main.php
+++ includes/layouts/sidebar_main.php
@@ -1,12 +1,19 @@
 <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
 	<div class="list-group">
-	  <a class="list-group-item active" href="#">Меню</a>
-	  <a class="list-group-item" href='../../index.php?id=projects'>Проекты МНПА</a>
-	  <a class="list-group-item" href='#'>МНПА</a>
-	  <a class="list-group-item" href='#'>Отчеты</a>
         <div class="dropdown">
-		<a aria-expanded="false" class="list-group-item dropdown-toggle" data-toggle="dropdown" href="#">
+		<a aria-expanded="false" class="list-group-item list-group-item-info dropdown-toggle" data-toggle="dropdown" href="#">
 		  Настройка <span class="caret"></span>
+		</a>
+		<ul class="dropdown-menu">
+		  <li><a href="../../index.php?id=users">Пользователи</a>
+		  <li class="divider"></li>
+		  <li><a href="../../index.php?id=status-project">Статусы проектов</a>
+		  <li><a href="../../index.php?id=type-mnpa">Типы МНПА</a>
+		</ul>
+        </div>       
+        <div class="dropdown">
+		<a aria-expanded="false" class="list-group-item list-group-item-info dropdown-toggle" data-toggle="dropdown" href="#">
+		  Справочники <span class="caret"></span>
 		</a>
 		<ul class="dropdown-menu">
 		  <li><a href="../../index.php?id=otdels">Отделы</a>
@@ -17,5 +24,26 @@
 		  <li><a href="../../index.php?id=type-mnpa">Типы МНПА</a>
 		</ul>
         </div>
+        <div class="dropdown">
+		<a aria-expanded="false" class="list-group-item list-group-item-info dropdown-toggle" data-toggle="dropdown" href="#">
+		  Операции <span class="caret"></span>
+		</a>
+		<ul class="dropdown-menu">
+		  <li><a href="../../index.php?id=projects">Проекты МНПА</a>
+		  <li><a href="../../index.php?id=mnpa">МНПА</a>
+		  <li><a href="../../index.php?id=v-registr">Передача МНПА</a>
+		</ul>
+        </div>
+        <div class="dropdown">
+		<a aria-expanded="false" class="list-group-item list-group-item-info dropdown-toggle" data-toggle="dropdown" href="#">
+		  Отчеты <span class="caret"></span>
+		</a>
+		<ul class="dropdown-menu">
+		  <li><a href="../../index.php?id=o-podgotovke">О подготовке проектов</a>
+		  <li><a href="../../index.php?id=o-sotrudnikah-v-proekte">О сотрудниках, участвующих в проектах</a>
+		  <li><a href="../../index.php?id=o-srokah">О превышении сроков проектов</a>
+		</ul>
+        </div>        
+    <a class="list-group-item list-group-item-info" href="#">Выход</a>    
 	</div>
 </div>
